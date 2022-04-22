fb50017194c98ab45e70d43931e33b9f8a5ab6ec
reuben <creubend@gmail.com>
2022-04-22 10:35:02 -0600

    clean out more github repo test

--- spec/facades/repository_facade_spec.rb
+++ spec/facades/repository_facade_spec.rb
@@ -1,8 +1,5 @@
 require "rails_helper"
 
 RSpec.describe RepositoryFacade do
-  it "creates repo poros", :vcr do
-    repo = RepositoryFacade.create_repo
-    expect(repo).to be_a(Repository)
-  end
+ 
 end
