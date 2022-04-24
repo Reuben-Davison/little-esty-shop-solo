9f6f47eec243794df02e1cef93250c14c818911e
reuben <creubend@gmail.com>
2022-04-24 14:47:09 -0600

    feat: Discount show page created

--- spec/features/merchants/discounts/show_spec.rb
+++ spec/features/merchants/discounts/show_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe "Discount show page" do
         merch1 = Merchant.create!(name: "Rob")
         discount1 = merch1.discounts.create!(threshold: 10, percentage: 25)
         visit "merchants/#{merch1.id}/discounts/#{discount1.id}"
+        expect(page).to have_content("Threshold: 10")
+        expect(page).to have_content("Percentage: 25")
     end
 end
